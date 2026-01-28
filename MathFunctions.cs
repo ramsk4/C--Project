f792eceb27077c8a2356da9c1974e0e9172f0e32
Shriram Kulkarni <41006662+ramsk4@users.noreply.github.com>
2026-01-28 19:12:53 +0530

    Createed  MathFunctions.cs
    Createed  MathFunctions.cs

---
+++ MathFunctions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class MathFunctions
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int mult(int a , int b)
+        {
+            return a * b;
+        }
+
+        public static int div(int a , int b)
+        {
+            return a / b;
+
+        }
+
+    }
+}
